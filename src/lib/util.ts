1f663acd1209184e5cee24181b27379dac2ec72d
Cameron <cameron.hudsonj@gmail.com>
2018-09-21 19:32:28 -0700

    Started work on crypto endpoints

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,6 +1,14 @@
 export const BASE_URL = 'https://api.robinhood.com';
+export const BASE_URL_CRYPTO = 'https://nummus.robinhood.com';
+
 export const endpoints = {
     login: BASE_URL + '/oauth2/token/',
     logout: BASE_URL + '/api-token-logout/',
     accounts: BASE_URL + '/accounts/',
+};
+
+export const endpointsCrypto = {
+    currencyPairs: BASE_URL_CRYPTO + '/currency_pairs',
+    orders: BASE_URL_CRYPTO + '/orders',
+    quotes: BASE_URL + '/marketdata/forex/quotes/',
 };
